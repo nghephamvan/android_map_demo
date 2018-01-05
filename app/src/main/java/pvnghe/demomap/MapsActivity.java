8d0a06a0beb7b0af5b55f3cac7f2bea2f4b9c83b
Nghe Pham <ilovetayninh@gmail.com>
2018-01-05 11:20:54 +0700

    Add draw map and poligon

--- app/src/main/java/pvnghe/demomap/MapsActivity.java
+++ app/src/main/java/pvnghe/demomap/MapsActivity.java
@@ -1,36 +1,33 @@
 package pvnghe.demomap;
 
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Dash;
 import com.google.android.gms.maps.model.Dot;
 import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PatternItem;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-                                                                GoogleMap.OnPolylineClickListener,
-                                                                GoogleMap.OnPolygonClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
     private static final int POLYGON_STROKE_WIDTH_PX = 8;
     private static final int PATTERN_DASH_LENGTH_PX = 20;
     private static final int PATTERN_GAP_LENGTH_PX = 20;
@@ -41,6 +38,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     // Create a stroke pattern of a gap followed by a dot.
     private static final List<PatternItem> PATTERN_POLYGON_BETA = Arrays.asList(DOT, GAP, DASH, GAP);
 
+    private GoogleMap mMap;
+    private ArrayList<LatLng> fieldPoligons;
+    private Button btn_draw_poligon;
+
+    private boolean isDrawField = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +51,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        fieldPoligons = new ArrayList<LatLng>();
+
+        btn_draw_poligon = findViewById(R.id.btn_draw_poligon);
+        btn_draw_poligon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isDrawField = !isDrawField;
+                if (isDrawField) {
+                    btn_draw_poligon.setText(R.string.draw_poligon);
+                } else {
+                    btn_draw_poligon.setText(R.string.done);
+                }
+            }
+        });
+
         mapFragment.getMapAsync(this);
     }
 
@@ -65,17 +84,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (isDrawField) {
+            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng currentLaLatLng) {
+                    if (currentLaLatLng != null) {
+                        createFieldPoint(currentLaLatLng);
+                    }
+                }
+            });
+        }
+
         // Add a marker in Sydney and move the camera
         LatLng vietnamLatLng = new LatLng(10.852980, 106.626994);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(vietnamLatLng));
 
-        MarkerOptions markerOptions = new MarkerOptions()
+        /*MarkerOptions markerOptions = new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_round))
                 .title("Green field")
                 .draggable(true)
                 .position(vietnamLatLng)
                 .snippet("Author: Ame");
         mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(vietnamLatLng));
 
         // Instantiates a new Polygon object and adds points to define a rectangle
         PolygonOptions rectOptions = new PolygonOptions()
@@ -93,25 +123,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);
 
         mMap.setOnPolylineClickListener(this);
-        mMap.setOnPolygonClickListener(this);
+        mMap.setOnPolygonClickListener(this);*/
     }
 
-    @Override
-    public void onPolylineClick(Polyline polyline) {
-        // Flip from solid stroke to dotted stroke pattern.
-        if ((polyline.getPattern() == null) || (!polyline.getPattern().contains(DOT))) {
-            polyline.setPattern(PATTERN_POLYGON_BETA);
-        } else {
-            // The default pattern is a solid stroke.
-            polyline.setPattern(null);
+    private void createFieldPoint(LatLng currentLaLatLng) {
+        if (fieldPoligons == null) {
+            fieldPoligons = new ArrayList<LatLng>();
         }
 
-        Toast.makeText(this, "Route type " + polyline.getTag().toString(),
-                Toast.LENGTH_SHORT).show();
+        MarkerOptions markerOptions = new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_point))
+                .draggable(true)
+                .position(currentLaLatLng);
+
+        int startPoint = fieldPoligons.size() - 1;
+        fieldPoligons.add(currentLaLatLng);
+        if (fieldPoligons.size() > 1) {
+            createFieldMidPoint(fieldPoligons.get(startPoint), fieldPoligons.get(fieldPoligons.size() - 1));
+        } else if (fieldPoligons.size() > 2) {
+            createFieldArea(fieldPoligons);
+        }
+
+        mMap.addMarker(markerOptions);
     }
 
-    @Override
-    public void onPolygonClick(Polygon polygon) {
+    private void createFieldArea(ArrayList<LatLng> fieldPoligons) {
+        // Instantiates a new Polygon object and adds points to define a rectangle
+        PolygonOptions rectOptions = new PolygonOptions()
+                .addAll(fieldPoligons)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(100,51, 255, 51));
 
+        // Get back the mutable Polygon
+        Polygon polygon = mMap.addPolygon(rectOptions);
+        polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);
+
+        mMap.addPolygon(rectOptions);
+    }
+
+    private void createFieldMidPoint(LatLng startPoint, LatLng endPoint) {
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .clickable(true)
+                .add(startPoint, endPoint)
+                .color(Color.GREEN)
+                .geodesic(true);
+
+        LatLngBounds bounds = new LatLngBounds(startPoint, endPoint);
+
+        LatLng midPoint = bounds.getCenter();
+        MarkerOptions markerOptions = new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_mid_point))
+                .draggable(true)
+                .position(midPoint);
+        mMap.addMarker(markerOptions);
+        mMap.addPolyline(polylineOptions);
     }
 }
