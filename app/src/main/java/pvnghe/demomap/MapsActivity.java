608c7099640642e5a03daa84add06985bfb9a6e2
Nghe Pham <ilovetayninh@gmail.com>
2018-01-05 16:09:03 +0700

    Update polygon in google map

--- app/src/main/java/pvnghe/demomap/MapsActivity.java
+++ app/src/main/java/pvnghe/demomap/MapsActivity.java
@@ -1,10 +1,13 @@
 package pvnghe.demomap;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,8 +19,10 @@ import com.google.android.gms.maps.model.Dot;
 import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.PointOfInterest;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -39,8 +44,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final List<PatternItem> PATTERN_POLYGON_BETA = Arrays.asList(DOT, GAP, DASH, GAP);
 
     private GoogleMap mMap;
+    private PolygonOptions polygonOptions;
+    private Polygon polygon;
     private ArrayList<LatLng> fieldPoligons;
     private Button btn_draw_poligon;
+    private ImageView iv_current_position;
+    private SupportMapFragment mapFragment;
 
     private boolean isDrawField = false;
 
@@ -49,21 +58,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
         fieldPoligons = new ArrayList<LatLng>();
 
         btn_draw_poligon = findViewById(R.id.btn_draw_poligon);
+        iv_current_position = findViewById(R.id.iv_current_position);
+
         btn_draw_poligon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isDrawField = !isDrawField;
-                if (isDrawField) {
+                if (btn_draw_poligon.getText().toString().equals(getString(R.string.done))) {
+                    isDrawField = false;
                     btn_draw_poligon.setText(R.string.draw_poligon);
                 } else {
+                    isDrawField = true;
                     btn_draw_poligon.setText(R.string.done);
                 }
+            }
+        });
+
+        iv_current_position.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LatLng vietnamLatLng = new LatLng(10.852980, 106.626994);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(vietnamLatLng));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(vietnamLatLng, 18));
             }
         });
 
@@ -83,31 +104,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        if (isDrawField) {
-            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                @Override
-                public void onMapClick(LatLng currentLaLatLng) {
-                    if (currentLaLatLng != null) {
-                        createFieldPoint(currentLaLatLng);
-                    }
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng currentLaLatLng) {
+                if (currentLaLatLng != null && isDrawField) {
+                    createFieldPoint(currentLaLatLng);
                 }
-            });
-        }
+            }
+        });
 
         // Add a marker in Sydney and move the camera
         LatLng vietnamLatLng = new LatLng(10.852980, 106.626994);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(vietnamLatLng));
 
-        /*MarkerOptions markerOptions = new MarkerOptions()
+        MarkerOptions markerOptions = new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_round))
                 .title("Green field")
+                .anchor(0.5f, 0.5f)
                 .draggable(true)
                 .position(vietnamLatLng)
                 .snippet("Author: Ame");
         mMap.addMarker(markerOptions);
 
-        // Instantiates a new Polygon object and adds points to define a rectangle
+
+        /*// Instantiates a new Polygon object and adds points to define a rectangle
         PolygonOptions rectOptions = new PolygonOptions()
                 .add(new LatLng(10.852980, 106.626994),
                         new LatLng(10.854287, 106.626383),
@@ -120,10 +140,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Get back the mutable Polygon
         Polygon polygon = mMap.addPolygon(rectOptions);
-        polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);
-
-        mMap.setOnPolylineClickListener(this);
-        mMap.setOnPolygonClickListener(this);*/
+        polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);*/
     }
 
     private void createFieldPoint(LatLng currentLaLatLng) {
@@ -134,14 +151,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         MarkerOptions markerOptions = new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_point))
                 .draggable(true)
+                .anchor(0.5f, 0.5f)
                 .position(currentLaLatLng);
 
         int startPoint = fieldPoligons.size() - 1;
         fieldPoligons.add(currentLaLatLng);
         if (fieldPoligons.size() > 1) {
             createFieldMidPoint(fieldPoligons.get(startPoint), fieldPoligons.get(fieldPoligons.size() - 1));
-        } else if (fieldPoligons.size() > 2) {
-            createFieldArea(fieldPoligons);
+            if (fieldPoligons.size() > 2) {
+                createFieldMidPoint(fieldPoligons.get(0), fieldPoligons.get(fieldPoligons.size() - 1));
+                createFieldArea(fieldPoligons);
+            }
         }
 
         mMap.addMarker(markerOptions);
@@ -149,16 +169,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void createFieldArea(ArrayList<LatLng> fieldPoligons) {
         // Instantiates a new Polygon object and adds points to define a rectangle
-        PolygonOptions rectOptions = new PolygonOptions()
-                .addAll(fieldPoligons)
-                .strokeColor(Color.GREEN)
-                .fillColor(Color.argb(100,51, 255, 51));
+        if (polygon == null) {
+            polygon = mMap.addPolygon(new PolygonOptions()
+                    .addAll(fieldPoligons)
+                    .strokeColor(Color.GREEN)
+                    .fillColor(Color.argb(100, 51, 255, 51)));
+        } else {
+            polygon.remove();
+            polygon = mMap.addPolygon(new PolygonOptions()
+                    .addAll(fieldPoligons)
+                    .strokeColor(Color.GREEN)
+                    .fillColor(Color.argb(100, 51, 255, 51)));
+        }
 
-        // Get back the mutable Polygon
-        Polygon polygon = mMap.addPolygon(rectOptions);
         polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);
-
-        mMap.addPolygon(rectOptions);
     }
 
     private void createFieldMidPoint(LatLng startPoint, LatLng endPoint) {
@@ -168,14 +192,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .color(Color.GREEN)
                 .geodesic(true);
 
-        LatLngBounds bounds = new LatLngBounds(startPoint, endPoint);
+        LatLng midPoint = getMiddleLatLng(startPoint, endPoint);
 
-        LatLng midPoint = bounds.getCenter();
         MarkerOptions markerOptions = new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_mid_point))
                 .draggable(true)
+                .anchor(0.5f, 0.5f)
                 .position(midPoint);
+
         mMap.addMarker(markerOptions);
         mMap.addPolyline(polylineOptions);
+    }
+
+    @NonNull
+    private LatLng getMiddleLatLng(LatLng startPoint, LatLng endPoint) {
+        double midLat = (startPoint.latitude + endPoint.latitude) / 2;
+        double midlon = (startPoint.longitude + endPoint.longitude) / 2;
+        return new LatLng(midLat, midlon);
     }
 }
